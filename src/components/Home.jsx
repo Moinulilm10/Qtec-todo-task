0ba32a6e18bf440972907ed8d26896b9b89745de
Moinul Islam <moinulilm10@gmail.com>
2024-02-06 20:08:27 +0600

    feature: home implemented

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,84 @@
+import { useContext } from "react";
+import { FaCheck } from "react-icons/fa";
+import { RxPlus } from "react-icons/rx";
+import { useNavigate } from "react-router-dom";
+import DisplayTodos from "../Utils/DisplayTodos";
+import Greeting from "../Utils/Greeting";
+import DataContext from "../context/DataContext";
+
 const Home = () => {
+  const {
+    data,
+    setData,
+    edit,
+    setEdit,
+    addNotificationTitle,
+    editNotificationTitle,
+    deleteNotificationTitle,
+    setDeleteNotificationTitle,
+    addNotification,
+    editNotification,
+    deleteNotification,
+    setDeleteNotification,
+    setTaskDetails,
+  } = useContext(DataContext);
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>home</h1>
+    <div className=" w-full relative min-h-screen pb-60 bg-gradient-to-r from-purple-700 to-purple-500">
+      <div className=" max-w-[1300px] px-3 m-auto">
+        <div>
+          <Greeting />
+
+          <div className="flex items-center justify-center gap-16">
+            <h1 className="font-bold text-3xl text-[#fff]">Add Task </h1>
+            <div
+              onClick={() => navigate("/addTodo")}
+              className="w-16 h-16 max-sm:w-14 max-sm:h-14 cursor-pointer bg-white grid place-items-center rounded-full left-[50%] -translate-x-[50%]"
+            >
+              <RxPlus className=" text-4xl max-sm:text-3xl plusIcon" />
+            </div>
+          </div>
+
+          <DisplayTodos
+            data={data}
+            setData={setData}
+            edit={edit}
+            setEdit={setEdit}
+            setDeleteNotificationTitle={setDeleteNotificationTitle}
+            setDeleteNotification={setDeleteNotification}
+            setTaskDetails={setTaskDetails}
+          />
+
+          {addNotification && (
+            <div className=" min-sm:w-[230px] px-3 py-2 rounded-md bg-white border-l-[10px] max-sm:border-l-8 flex items-center gap-2 border-green-600 fixed bottom-32 left-[50%] -translate-x-[50%]">
+              <FaCheck className=" text-2xl max-sm:text-xl text-green-500" />
+              <h2 className=" max-md:text-xs text-sm text-slate-600 font-semibold">
+                Added task - {addNotificationTitle}
+              </h2>
+            </div>
+          )}
+
+          {editNotification && (
+            <div className=" min-sm:w-[230px] px-3 py-2 rounded-md bg-white border-l-[10px] max-sm:border-l-8 flex items-center gap-2 border-green-600 fixed bottom-32 left-[50%] -translate-x-[50%]">
+              <FaCheck className=" text-2xl max-sm:text-xl text-green-500" />{" "}
+              <h2 className=" max-md:text-xs text-sm text-slate-600 font-semibold">
+                Task {editNotificationTitle} updated
+              </h2>
+            </div>
+          )}
+
+          {deleteNotification && (
+            <div className=" min-sm:w-[230px] px-3 py-2 rounded-md bg-white border-l-[10px] max-sm:border-l-8 flex items-center gap-2 border-green-600 fixed bottom-32 left-[50%] -translate-x-[50%]">
+              <FaCheck className=" text-2xl max-sm:text-xl text-green-500" />
+              <h2 className="max-md:text-xs text-sm text-slate-600 font-semibold">
+                Deleted Task - {deleteNotificationTitle}
+              </h2>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -8,6 +8,13 @@ export const DataProvider = ({ children }) => {
   const [data, setData] = useState([]);
   const [index, setIndex] = useState(null);
 
+  const [addNotificationTitle, setAddNotificationTitle] = useState("");
+  const [editNotificationTitle, setEditNotificationTitle] = useState("");
+  const [deleteNotificationTitle, setDeleteNotificationTitle] = useState("");
+  const [addNotification, setAddNotification] = useState(false);
+  const [editNotification, setEditNotification] = useState(false);
+  const [deleteNotification, setDeleteNotification] = useState(false);
+
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem("todoItems"));
     setData(items || []);
@@ -22,6 +29,18 @@ export const DataProvider = ({ children }) => {
         setEdit,
         index,
         setIndex,
+        addNotificationTitle,
+        editNotificationTitle,
+        deleteNotificationTitle,
+        setDeleteNotificationTitle,
+        addNotification,
+        editNotification,
+        deleteNotification,
+        setDeleteNotification,
+        setAddNotificationTitle,
+        setAddNotification,
+        setEditNotificationTitle,
+        setEditNotification,
       }}
     >
       {children}
